32100ca7546b928a5ec51a145c2312e9a2833001
mathieu-pc <116907979+mathieu-pc@users.noreply.github.com>
2025-08-26 18:11:17 -0400

    Update PostProcessing.getNoiseCube
    I thought gaussian_filter mode "reflect" was "wrap", but reflection causes pixels to auto-correlate which increases the noise on the borders. Auto-correlation increases the noise standard deviation so this was changed.

--- Mochi/PostProcessing.py
+++ Mochi/PostProcessing.py
@@ -17,7 +17,7 @@ def getNoiseCube(shape, noiseRMS, beam, pixelSize, spectralSigma = 0):
 	beamSigma = convertBeamToBeamSigma(beam, spectralSigma, pixelSize)
 	noiseRMSPreBeam = noiseRMS * 2 * np.sqrt(beamSigma[1] * beamSigma[2]) * np.sqrt(np.pi)
 	cube = np.random.normal(loc = 0, scale = noiseRMSPreBeam.value, size = shape)
-	cube = gaussian_filter(cube, beamSigma, mode = "reflect")
+	cube = gaussian_filter(cube, beamSigma, mode = "wrap")
 	return cube * noiseRMSPreBeam.unit
 
 def convolve(cube, beam, pixelSize, spectralSigma = 0, pad = 2):
@@ -36,3 +36,4 @@ def getJyFromMass(cube, beam, pixelSize, channelWidth, distance):
 	converter = getMassFromFlux(1 * units.Jy, beam, pixelSize, channelWidth, distance)
 
 	return (cube / converter).decompose() * units.Jy / units.beam
+
