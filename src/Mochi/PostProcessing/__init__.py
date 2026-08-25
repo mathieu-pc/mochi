304d2ab4171e9547e47ca79295025241ea7f0dba
Mathieu Perron-Cormier <116907979+mathieu-pc@users.noreply.github.com>
2026-08-25 17:57:27 -0400

    Update PostProcessing.__init__.py
    Add getMassFromFlux

--- src/Mochi/PostProcessing/__init__.py
+++ src/Mochi/PostProcessing/__init__.py
@@ -1,5 +1,5 @@
 from .cardhandler import makeCards
 from .utils import _astropyUnitWrap
 from .convolve import convolve
-from .unitconvert import getJyFromMass
+from .unitconvert import getJyFromMass, getMassFromFlux
 from .noise import getNoiseCube
